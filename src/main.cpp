35d8e67a2d48a659e1f968153ba12ac045ebc930
yash <yashchaurasia630@gmail.com>
2026-08-27 14:14:03 +0530

    restart

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,5 @@
-#include "app/first_app.hpp"
-#include <cstdlib>
-#include <exception>
 #include <iostream>
 
 int main() {
-  lve::FirstApp app;
-
-  try {
-    app.run();
-  } catch (std::exception &e) {
-    std::cerr << "ERROR: " << e.what() << std::endl;
-    return EXIT_FAILURE;
-  }
-
-  return EXIT_SUCCESS;
+  std::cout << "Hello world" << std::endl;
 }
